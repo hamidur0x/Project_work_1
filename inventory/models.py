19387e7be9c497c355a00f367b8fb7e6c4db80b3
hamidur0x <hamidurrahman0705@gmail.com>
2026-07-13 19:20:20 +0600

    done 100%

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50, choices=[('Rice', 'Rice'), ('Sope', 'Sope'), ('Milk', 'Milk'),('Onions','Onions'), ('Garlic','Garlic'),('Potatoes','Potatoes'),('Peppers','Peppers'),('Bananas','Bananas'),('Apples','Apples'),('Oil','Oil')])
     stock = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100, choices=[('Frozen Foods',  'Frozen Foods'),('Meat', 'Meat'), ('Bakery','Bakery'),('Household', 'Household'), ('Dry Goods', 'Dry Goods'), ('Clothing', 'Clothing'), ('Snacks', 'Snacks')])
     unit = models.CharField(max_length=50)
